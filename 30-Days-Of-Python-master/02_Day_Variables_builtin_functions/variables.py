cd13a406388325b8d211342b2621594f92326cd3
Clinton Ojo <clintonojo69@gmail.com>
2024-12-28 11:37:03 +0000

    Initial commit

---
+++ 30-Days-Of-Python-master/02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,44 @@
+#Day 2: 30 Days of python programming
+first_name="Clinton"
+last_name="ojo"
+full_name="ClintonOjo"
+country="Ireland"
+city="dublin"
+age=20
+year=2004
+is_married="no"
+is_true=True
+is_light_on=False
+
+first_name,last_name,country,age,is_married = 'Clinton', 'Ojo', 'Ireland', 20, False
+print(type(first_name))
+print(type(country))
+print(type(is_married,))
+print(type(last_name))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(len(first_name))
+print(len(last_name))
+num_one=5
+num_two=4
+print(num_one+num_two)
+total=num_two+num_two
+diff=num_two-num_one
+product=num_two*num_one
+remainder=num_one%num_two
+floor_divison=num_one//num_two
+_area_of_cirle_=3.14 *30*30
+_circum_of_circle_=2*3.14*30
+rad=float(input('what is the rad'))
+area=3.14 * rad* rad
+print(area)
+first_name=(input("What is your first name"))
+last_name=(input("What is your last name"))
+country=(input("what country are you from"))
+age=int(input("What age are you"))
+
+
+
